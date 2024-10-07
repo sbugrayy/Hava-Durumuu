505f43b94b93e26b0f0a5f3d1d23dfa4a3132c51
sbugrayy <bugrabugra511@gmail.com>
2024-10-07 20:10:52 +0300

    Hava Durumu
    Windows Forms ile geliştirilmiştir.

---
+++ HavaDurumu/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace HavaDurumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TB1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.L1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.L2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.L3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TB1
+            // 
+            resources.ApplyResources(this.TB1, "TB1");
+            this.TB1.BorderColor = System.Drawing.Color.Black;
+            this.TB1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TB1.DefaultText = "";
+            this.TB1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TB1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TB1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TB1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TB1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TB1.ForeColor = System.Drawing.Color.Black;
+            this.TB1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TB1.Name = "TB1";
+            this.TB1.PasswordChar = '\0';
+            this.TB1.PlaceholderForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.TB1.PlaceholderText = "";
+            this.TB1.SelectedText = "";
+            this.TB1.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
+            // 
+            // L1
+            // 
+            resources.ApplyResources(this.L1, "L1");
+            this.L1.BackColor = System.Drawing.Color.Transparent;
+            this.L1.Name = "L1";
+            // 
+            // L2
+            // 
+            resources.ApplyResources(this.L2, "L2");
+            this.L2.BackColor = System.Drawing.Color.Transparent;
+            this.L2.Name = "L2";
+            this.L2.Click += new System.EventHandler(this.guna2HtmlLabel2_Click);
+            // 
+            // L3
+            // 
+            resources.ApplyResources(this.L3, "L3");
+            this.L3.BackColor = System.Drawing.Color.Transparent;
+            this.L3.Name = "L3";
+            // 
+            // guna2Button1
+            // 
+            resources.ApplyResources(this.guna2Button1, "guna2Button1");
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.SystemColors.Highlight;
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::HavaDurumu.Properties.Resources.Hava_durumu;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.TB1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox TB1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HavaDurumu/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HavaDurumu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        public void islem1()
+        {
+            if (TB1.Text == "")
+                MessageBox.Show("Lütfen ilk önce bir lokasyon giriniz!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                Form1 frm = new Form1();
+
+                string htmlCode = "";
+
+                using(WebClient client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    htmlCode = client.DownloadString($"https://api.openweathermap.org/data/2.5/weather?q={TB1.Text}&appid=72187a2b0a597b5b1c986a0eb2f5ded9&lang-tr");
+                }
+
+                dynamic stuff = JObject.Parse(htmlCode);
+
+                dynamic sicaklik = stuff.main.temp;
+                int veriSicaklik = (sicaklik - 273);
+
+                L1.Text = veriSicaklik.ToString() + " °C";
+                L1.Location = new Point(frm.Size.Width / 2 - L1.Size.Width / 2 - 10, 140);
+
+                dynamic status = stuff.weather[0].description;
+
+                L2.Text = status;
+                L2.Location = new Point(frm.Size.Width / 2 - L2.Size.Width / 2 - 10, 201);
+
+                dynamic country = stuff.sys.country;
+                dynamic name = stuff.name;
+
+                L3.Text = $"{name}, {country}";
+                L3.Location = new Point(frm.Size.Width / 2 - L3.Size.Width / 2 - 10, 335);
+
+                TB1.Text = "";
+            }
+                
+             
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            L1.Text = "";
+            L2.Text = "";
+            L3.Text = "";
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Thread islem = new Thread(() => islem1());
+            islem.Start();
+        }
+    }
+}
